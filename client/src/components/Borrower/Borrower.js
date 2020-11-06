30c061bcf7072f3e427c75c97b28c577168b4495
agostinii1 <54629275+agostinii1@users.noreply.github.com>
2020-11-06 13:13:07 -0500

    test

--- client/src/components/Borrower/Borrower.js
+++ client/src/components/Borrower/Borrower.js
@@ -3,11 +3,12 @@ import React from 'react';
 /* Styles */
 import useStyles from './Borrower.styles';
 
+
 const Borrower = (props) => {
   const classes = useStyles(props);
   return (
     <div className={classes.root}>
-      Borrower...
+      Borrowers.....
     </div>
   );
 };
