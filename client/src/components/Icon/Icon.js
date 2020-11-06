3c2a0721775e68e0a7fb8318558a9daa327e5a7e
danielbarone <dev.danielbarone@gmail.com>
2020-11-05 22:31:25 -0500

    fix nav icons

--- client/src/components/Icon/Icon.js
+++ client/src/components/Icon/Icon.js
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 /* Styles */
 /* MUI - Icons */
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
 import Brightness4Icon from '@material-ui/icons/Brightness4';
@@ -11,19 +12,25 @@ import ChatIcon from '@material-ui/icons/Chat';
 import DevicesIcon from '@material-ui/icons/Devices';
 import EventIcon from '@material-ui/icons/Event';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
 import ListAltIcon from '@material-ui/icons/ListAlt';
 import LiveHelpIcon from '@material-ui/icons/LiveHelp';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
 import MenuIcon from '@material-ui/icons/Menu';
 import PaymentIcon from '@material-ui/icons/Payment';
 import PersonAddOutlinedIcon from '@material-ui/icons/PersonAddOutlined';
 import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import WcIcon from '@material-ui/icons/Wc';
 
 import useStyles from './Icon.styles';
 
 const icons = {
+  admin: SupervisorAccountIcon,
   avatar: AccountCircleIcon,
+  book: MenuBookIcon,
+  books: LibraryBooksIcon,
   calendar: EventIcon,
   chat: ChatIcon,
   creditCard: PaymentIcon,
@@ -31,6 +38,7 @@ const icons = {
   email: MailOutlineIcon,
   help: LiveHelpIcon,
   info: DevicesIcon,
+  library: AccountBalanceIcon,
   lightsOff: Brightness4Icon,
   lightsOn: Brightness7Icon,
   list: ListAltIcon,
