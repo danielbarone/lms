095be1c3cfcf20f1b32c30ba18c586f60de34cd1
danielbarone <dev.danielbarone@gmail.com>
2020-11-05 21:31:31 -0500

    rm stripe-service

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,7 +2,6 @@ import Admin from './Admin/Admin';
 import AuthButton from './Button/AuthButton';
 import AvatarBtn from './Button/AvatarBtn';
 import Borrower from './Borrower/Borrower';
-import BasicTable from './Table/BasicTable';
 import Dashboard from './Dashboard/Dashboard';
 import Home from './Home/Home';
 import Icon from './Icon/Icon';
@@ -13,7 +12,6 @@ import Orders from './Orders/Orders';
 import Preview from './Preview/Preview';
 import Register from './Register/Register';
 import Result from './Result/Result';
-import Svg from './Svg/Svg';
 import { Canceled, Checkout, Success } from './Checkout';
 
 export {
@@ -21,7 +19,6 @@ export {
   AuthButton,
   AvatarBtn,
   Borrower,
-  BasicTable,
   Canceled,
   Checkout,
   Dashboard,
@@ -35,5 +32,4 @@ export {
   Register,
   Result,
   Success,
-  Svg,
 };
