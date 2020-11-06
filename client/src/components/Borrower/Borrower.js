b55b90e733ac8c45d8b8dfc15103c60190c075a0
agostinii1 <54629275+agostinii1@users.noreply.github.com>
2020-11-06 14:33:34 -0500

    updaded app.props for the pass

--- client/src/components/Borrower/Borrower.js
+++ client/src/components/Borrower/Borrower.js
@@ -3,6 +3,12 @@ import React from 'react';
 /* Styles */
 import useStyles from './Borrower.styles';
 
+console.log("Borrower test");
+// const axios = require('axios');
+// axios.get("http://localhost:8080/getAllBranches").then(resp => {
+//   console.log("Test2:");
+//   console.log(resp);
+// });
 
 const Borrower = (props) => {
   const classes = useStyles(props);
