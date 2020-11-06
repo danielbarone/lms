9730717ea9f23782f3bd9f63d4949213faa0de5c
AveryJ13 <59802067+AveryJ13@users.noreply.github.com>
2020-11-06 09:58:15 -0700

    prepping for merge

--- client/src/components/Librarian/Librarian.js
+++ client/src/components/Librarian/Librarian.js
@@ -1,13 +1,40 @@
 /* React */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 /* Styles */
 import useStyles from './Librarian.styles';
 
 const Librarian = (props) => {
   const classes = useStyles(props);
+
+  const [results, setResults] = useState([]);
+  const [isLoaded, setLoaded] = useState(false);
+
+
+
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/getAllBranches').then(res => {
+      setResults(res.data)
+      console.log(res.data)
+    }).catch(err => console.log(err))
+  }, [])
+
+
+
   return (
+
     <div className={classes.root}>
       Librarian...
+      <div>
+        <ul>
+          {results.map(result => (
+            <li key={result.branchId}>
+              {result.branchName}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
