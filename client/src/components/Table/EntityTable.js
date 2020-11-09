48d56f9c52a8842cacb9b56a21b1adee5a3a1c92
danielbarone <dev.danielbarone@gmail.com>
2020-11-09 16:53:33 -0500

    modify null check

--- client/src/components/Table/EntityTable.js
+++ client/src/components/Table/EntityTable.js
@@ -80,7 +80,7 @@ const EntityTable = (props) => {
         />
       </div>
       <div className={classes.selectedContainer}>
-        {selection !== null ? (
+        {selection ? (
           selection.map((v, i) => (
             <CustomChip
               key={`selection-key-${i}`}
