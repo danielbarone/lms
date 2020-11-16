f4a0f8e7b1e6df7dd9febbb19b58f703bd6fe08c
danielbarone <dev.danielbarone@gmail.com>
2020-11-16 15:53:23 -0500

    save main.tf

--- terraform/node-server/main.tf
+++ terraform/node-server/main.tf
@@ -7,6 +7,30 @@ resource "aws_instance" "default" {
   subnet_id              = var.subnet-id
   vpc_security_group_ids = var.vpc-security-group-ids
 
+  user_data = <<EOF
+#!/bin/bash
+cd /tmp
+echo '#!/bin/bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.0/install.sh | bash
+source ~/.bashrc
+nvm i 12.13.0
+curl -o- -L https://yarnpkg.com/install.sh | bash
+sudo yum install -y ruby wget
+cd /home/ec2-user
+wget https://aws-codedeploy-ap-southeast-2.s3.ap-southeast-2.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo ln -s /home/ec2-user/.nvm/versions/node/v12.13.0/bin/node /usr/bin
+sudo ln -s /home/ec2-user/.nvm/versions/node/v12.13.0/bin/npm /usr/bin
+sudo ln -s /home/ec2-user/.yarn/bin/yarn /usr/bin
+sudo yarn global add pm2
+sudo ln -s /usr/local/bin/pm2 /usr/bin' >> init.sh
+chmod +x init.sh
+/bin/su -c "/tmp/init.sh" - ec2-user
+rm init.sh
+' >> init.sh
+EOF;
+
   tags = {
     Name = var.name
   }
