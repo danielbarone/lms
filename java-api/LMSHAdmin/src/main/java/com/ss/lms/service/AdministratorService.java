708871ada94aa529b3cd2ba32cbfe2d8e02b0ca4
danielbarone <dev.danielbarone@gmail.com>
2020-12-07 23:40:02 -0800

    fix admin service add book method

--- java-api/LMSHAdmin/src/main/java/com/ss/lms/service/AdministratorService.java
+++ java-api/LMSHAdmin/src/main/java/com/ss/lms/service/AdministratorService.java
@@ -228,21 +228,7 @@ public class AdministratorService {
 				 return(getAllBorrowers());
 		
 	}
-	
-	
-	@RequestMapping(value = "/addBookRE", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
-	public ResponseEntity<?> addBookRE(@RequestBody Book book) {
-		try {
-			brepo.save(book);
-			return new ResponseEntity<>(book, HttpStatus.OK);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new ResponseEntity<>("Failed to add book", HttpStatus.BAD_REQUEST);
-		}
-	}
-	
 
-	
 	@Transactional
 	@RequestMapping(value = "/deleteBook", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
 	public List<Book> deleteBook(@RequestBody Book book) throws SQLException { 
